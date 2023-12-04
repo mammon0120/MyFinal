f4686c6a46eea0c8bc972d6f3c50e5f11a207ea5
mammon0120 <mammon0120111@gmail.com>
2023-12-04 13:58:19 -0800

    update

--- game_run.py
+++ game_run.py
@@ -1,8 +1,10 @@
 import random
+from game_setting import Player
 
 
 # game parameters settings
 END_POINT = 30
+GAME_HISTORY = "game_history.txt"
 
 
 def roll_dice() -> int:
@@ -83,9 +85,89 @@ def check_hp(hp: int) -> bool:
         hp(int): the player's HP value
 
     Returns:
-        bool: if the game is over
+        bool: wheither the game is over
     """
     if hp <= 0:
         return True
     else:
         return False
+
+
+def load_history(filename=GAME_HISTORY) -> tuple:
+    """
+    Load the game history and get the latest game's information.
+
+    Example:
+        >>> load_history("test_history_1.txt")
+        Something Wrong with the game history. Please Start a New Game
+        ()
+        >>> load_history("test_history_2.txt")
+        ('jack', 50, 10)
+
+    Args:
+        filename(str): name of the file, default to be GAME_HISTORY
+    Returns:
+        tuple: the latest game's information
+    """
+    setting = []
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                if line.strip():
+                    setting.append(line.strip())
+        if len(setting) != 3:
+            print("Something Wrong with the game history. Please Start a New Game")
+            return ()
+        else:
+            name = setting[0]
+            game_hp = int(setting[1])
+            position = int(setting[2])
+            return name, game_hp, position
+    except FileNotFoundError:
+        print("Game History is missing! Please Start a New Game")
+    except PermissionError:
+        print("You don't get the permission of the setting file.")
+    except IOError:
+        print("Error!")
+
+
+def clean_history(filename=GAME_HISTORY) -> None:
+    """
+    Clean the old game history.
+
+    Args:
+        filename(str): name of the file, default to be GAME_HISTORY
+    Returns:
+        None
+    """
+    try:
+        with open(filename, 'w') as file:
+            file.truncate(0)
+    except FileNotFoundError:
+        print("Game History is missing!")
+    except PermissionError:
+        print("You don't get the permission of the setting file.")
+    except IOError:
+        print("Error!")
+
+
+def save_game_history(player: Player) -> None:
+    """
+    Save this game history.
+
+    Args:
+        player(Player): player information
+    Returns:
+        None
+    """
+    try:
+        with open(GAME_HISTORY, 'w') as file:
+            file.write(player.name + '\n')
+            file.write(str(player.game_hp) + '\n')
+            file.write(str(player.position))
+    except FileNotFoundError:
+        print("Game History is missing!")
+    except PermissionError:
+        print("You don't get the permission of the setting file.")
+    except IOError:
+        print("Error!")
